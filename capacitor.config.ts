8d2ce82dda3ac255cb79ee7d6f3c4af335b2744a
Nil Arroyo <nil.arroyo@altia.es>
2026-04-07 10:17:00 +0200

    change: update capacitor appId from ionic default to wallet bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.eudistack.wallet',
   appName: 'WalletIN2',
   webDir: 'www',
   bundledWebRuntime: false
