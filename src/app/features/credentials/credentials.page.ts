53fc53044f3a0a562f1ed38ecc8a8bf95b3c6319
Aina Gomez <93551599+aina-gomez@users.noreply.github.com>
2026-05-11 09:20:42 +0200

    Feature/support rp metadata wallet (#88)
    * feat: support rp metadata wallet
    
    * feat: display verifier metadata in vc-selector component
    
    * fix: resolve logo visibility issues in dark mode
    
    * feat: add tests for vc-selector.page
    
    * docs: changelog update and remove logs

--- src/app/features/credentials/credentials.page.ts
+++ src/app/features/credentials/credentials.page.ts
@@ -401,6 +401,7 @@ export class CredentialsPage implements OnInit, ViewWillLeave {
           clientId: authRequest.clientId,
           dcqlQuery: authRequest.dcqlQuery,
           selectableVcList,
+          clientMetadata: authRequest.clientMetadata,
         };
 
         return from(
