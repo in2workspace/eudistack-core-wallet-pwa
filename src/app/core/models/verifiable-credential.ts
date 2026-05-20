8f83b7e7bce8115406b50f752129d5e1d393a082
rogermiretin2 <roger.miret@altia.es>
2026-05-20 17:05:38 +0200

    Feature/update label to version two (#106)
    * feat(label): update to gx.labelcredential.w3c.2
    
    * core: update version

--- src/app/core/models/verifiable-credential.ts
+++ src/app/core/models/verifiable-credential.ts
@@ -22,7 +22,7 @@ export const CREDENTIAL_TYPES_ARRAY = [
   'learcredential.employee.sd.1',
   'learcredential.machine.w3c.3',
   'learcredential.machine.sd.1',
-  'gx.labelcredential.w3c.1',
+  'gx.labelcredential.w3c.2',
   'doctorid.sd.1',
 ] as const;
 export type CredentialType = typeof CREDENTIAL_TYPES_ARRAY[number];
