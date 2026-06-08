5d1f0bc16dd54824ab91fb392f6c341b94bb52c4
aina-gomez <gomezp.aina@gmail.com>
2026-06-08 15:40:22 +0200

    test: uninstall playwright

--- e2e/auth/exploration.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test.only('exploration', async ({ page }) => {
-  await page.goto('https://dome.127.0.0.1.nip.io:4443/wallet/');
-  await page.pause();
-});
