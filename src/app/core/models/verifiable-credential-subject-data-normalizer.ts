be0327ba6c111dd95d1c1583b7a94c3df315b1ba
David Segura Saumell <dsegurasmll@gmail.com>
2026-06-15 09:28:35 +0200

    fix: add legacy credential types to wallet mappings

--- src/app/core/models/verifiable-credential-subject-data-normalizer.ts
+++ src/app/core/models/verifiable-credential-subject-data-normalizer.ts
@@ -32,6 +32,8 @@ export class VerifiableCredentialSubjectDataNormalizer {
   }
 
   private normalizerMapByCredentialType: Record<CredentialType, (s: CredentialSubject) => CredentialSubject> = {
+    'LEARCredentialEmployee': (s: CredentialSubject) => this.normalizeMandateSubject(s, true),
+    'LEARCredentialMachine': (s: CredentialSubject) => this.normalizeMandateSubject(s, false),
     'learcredential.employee.w3c.4': (s: CredentialSubject) => this.normalizeMandateSubject(s, true),
     'learcredential.employee.sd.1': (s: CredentialSubject) => this.normalizeMandateSubject(s, true),
     'learcredential.machine.w3c.3': (s: CredentialSubject) => this.normalizeMandateSubject(s, false),
